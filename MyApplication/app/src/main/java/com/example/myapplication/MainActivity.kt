6a96f6fc99e5624c5199eb578de5af800748882f
Sushma1997 <sushee.reddy1997@gmail.com>
2021-11-22 02:39:00 -0600

    added fworking functions

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -129,5 +129,167 @@ class MainActivity : AppCompatActivity() {
         bbrac2.setOnClickListener {
             tvMain.text = (tvMain.text.toString() + ")")
         }
+        bpi.setOnClickListener {
+            // on clicking on pi button we are adding
+            // pi value as 3.142 to our current value.
+            tvMain.text = (tvMain.text.toString() + "3.142")
+            tvsec.text = (bpi.text.toString())
+        }
+        bsin.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "sin")
+        }
+        bcos.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "cos")
+        }
+        btan.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "tan")
+        }
+        binv.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "^" + "(-1)")
+        }
+        bln.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "ln")
+        }
+        blog.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "log")
+        }
+
+        bminus.setOnClickListener {
+            // on clicking on minus we are checking if
+            // the user has already a minus operation on screen.
+            // if minus operation is already present
+            // then we will not do anything.
+            val str: String = tvMain.text.toString()
+            if (!str.get(index = str.length - 1).equals("-")) {
+                tvMain.text = (tvMain.text.toString() + "-")
+            }
+        }
+        bmul.setOnClickListener {
+            // if mul sign is not present in our
+            // text view then only we are adding
+            // the multiplication operator to it.
+            val str: String = tvMain.text.toString()
+            if (!str.get(index = str.length - 1).equals("*")) {
+                tvMain.text = (tvMain.text.toString() + "*")
+            }
+        }
+        bsqrt.setOnClickListener {
+            if (tvMain.text.toString().isEmpty()) {
+                // if the entered number is empty we are displaying an error message.
+                Toast.makeText(this, "Please enter a valid number..", Toast.LENGTH_SHORT).show()
+            } else {
+                val str: String = tvMain.text.toString()
+                // on below line we are calculation
+                // square root of the given number.
+                val r = Math.sqrt(str.toDouble())
+                // on below line we are converting our double
+                // to string and then setting it to text view.
+                val result = r.toString()
+                tvMain.setText(result)
+            }
+        }
+        bequal.setOnClickListener {
+            val str: String = tvMain.text.toString()
+            // on below line we are calling an evaluate
+            // method to calculate the value of expressions.
+            val result: Double = evaluate(str)
+            // on below line we are getting result
+            // and setting it to text view.
+            val r = result.toString()
+            tvMain.setText(r)
+            tvsec.text = str
+        }
+        bac.setOnClickListener {
+            // on clicking on ac button we are clearing
+            // our primary and secondary text view.
+            tvMain.setText("")
+            tvsec.setText("")
+        }
+        bc.setOnClickListener {
+            // on clicking on c button we are clearing
+            // the last character by checking the length.
+            var str: String = tvMain.text.toString()
+            if (!str.equals("")) {
+                str = str.substring(0, str.length - 1)
+                tvMain.text = str
+            }
+        }
+        bsquare.setOnClickListener {
+            if (tvMain.text.toString().isEmpty()) {
+                // if the entered number is empty we are displaying an error message.
+                Toast.makeText(this, "Please enter a valid number..", Toast.LENGTH_SHORT).show()
+            } else {
+                // on below line we are getting the expression and then calculating the square of the number
+                val d: Double = tvMain.getText().toString().toDouble()
+                // on below line we are calculating the square.
+                val square = d * d
+                // after calculating the square we
+                // are setting it to text view.
+                tvMain.setText(square.toString())
+                // on below line we are setting
+                // the d to secondary text view.
+                tvsec.text = "$d²"
+            }
+        }
+        bfact.setOnClickListener {
+            if (tvMain.text.toString().isEmpty()) {
+                // if the entered number is empty we are displaying an error message.
+                Toast.makeText(this, "Please enter a valid number..", Toast.LENGTH_SHORT).show()
+            } else {
+                // on below line we are getting int value
+                // and calculating the factorial value of the entered number.
+                val value: Int = tvMain.text.toString().toInt()
+                val fact: Int = factorial(value)
+                tvMain.setText(fact.toString())
+                tvsec.text = "$value`!"
+            }
+
+        }
+
+    }
+
+    fun factorial(n: Int): Int {
+        // this method is use to find factorial
+        return if (n == 1 || n == 0) 1 else n * factorial(n - 1)
+    }
+
+    fun evaluate(str: String): Double {
+        return object : Any() {
+            // on below line we ar creating variable
+            // for tracking the position and char pos.
+            var pos = -1
+            var ch = 0
+
+            // below method is for moving to next character.
+            fun nextChar() {
+                // on below line we are incrementing our position
+                // and moving it to next position.
+                ch = if (++pos < str.length) str[pos].toInt() else -1
+            }
+
+            // this method is use to check the extra space
+            // present int the expression and removing it.
+            fun eat(charToEat: Int): Boolean {
+                while (ch == ' '.toInt()) nextChar()
+                // on below line we are checking the char pos
+                // if both is equal then we are returning it to true.
+                if (ch == charToEat) {
+                    nextChar()
+                    return true
+                }
+                return false
+            }
+
+            // below method is to parse our
+            // expression and to get the ans
+            // in this we are calling a parse
+            // expression method to calculate the value.
+            fun parse(): Double {
+                nextChar()
+                val x = parseExpression()
+                if (pos < str.length) throw RuntimeException("Unexpected: " + ch.toChar())
+                return x
+            }
+
     }
 } 
